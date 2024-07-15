3707fa805d8561ade00e2eb64c4f4c8199298df0
Umrao Bisht <umraobisht1@gmail.com>
2024-07-15 22:47:34 +0530

    Added Generic type

--- build/js/index-signature-and-typeof-assertions.js
+++ build/js/index-signature-and-typeof-assertions.js
@@ -20,3 +20,29 @@ const todaysNet = (transactions) => {
     return total;
 };
 console.log(todaysNet(todaysTransaction));
+// todaysTransaction.Pizza = 30;
+console.log(todaysTransaction['umrao']);
+const student = {
+    name: "John",
+    GPA: 7.9,
+    classes: [100, 200]
+};
+// console.log(student.test);
+for (const key in student) {
+    console.log(`${key}:${student[key]}`);
+}
+Object.keys(student).map((key) => {
+    console.log(`${student[key]}`);
+});
+// console.log(typeof Student);
+const logStudentKey = (student, key) => {
+    console.log(`${key}:${student[key]}`);
+};
+const monthlyIncome = {
+    salary: 1000,
+    bonus: '150',
+    sidehustle: 100
+};
+for (const revenue in monthlyIncome) {
+    console.log(`${revenue}:${monthlyIncome[revenue]}`);
+}
--- src/index-signature-and-typeof-assertions.ts
+++ src/index-signature-and-typeof-assertions.ts
@@ -1,13 +1,18 @@
 // index signature 
 // typeof assertions
 
+
 // interface TransactionObj {
-//     Pizza: number
-//     Books: number
-//     Job: number
+//     readonly [index: string]: number
 // }
+
+
 interface TransactionObj {
-    [index: string]: number
+    readonly [index: string]: number
+    Pizza: number
+    Books: number
+    Job: number
+    // umrao:"Hello"
 }
 
 const todaysTransaction: TransactionObj = {
@@ -29,11 +34,64 @@ const todaysNet = (transactions: TransactionObj): number => {
     for (const transaction in transactions) {
         total += transactions[transaction];
     }
-
     return total;
 }
 
 console.log(todaysNet(todaysTransaction));
 
+// todaysTransaction.Pizza = 30;
+
+console.log(todaysTransaction['umrao']);
 
 
+
+
+interface Student {
+    // [key: string]: number | string | number[] | undefined
+    name: string
+    GPA: number,
+    classes?: number[]
+}
+
+const student: Student = {
+    name: "John",
+    GPA: 7.9,
+    classes: [100, 200]
+}
+
+// console.log(student.test);
+
+for (const key in student) {
+    console.log(`${key}:${student[key as keyof Student]}`)
+}
+
+Object.keys(student).map((key) => {
+    console.log(`${student[key as keyof typeof student]}`);
+
+})
+
+// console.log(typeof Student);
+
+const logStudentKey = (student: Student, key: keyof Student): void => {
+    console.log(`${key}:${student[key]}`);
+}
+
+// logStudentKey(student, 'GPA');
+
+
+// interface Income{
+//     [key: string]: number | string
+// }
+
+type Streams = 'salary' | 'bonus' | 'sidehustle'
+type Incomes = Record<Streams, number | string>
+
+const monthlyIncome: Incomes = {
+    salary: 1000,
+    bonus: '150',
+    sidehustle: 100
+}
+
+for (const revenue in monthlyIncome) {
+    console.log(`${revenue}:${monthlyIncome[revenue as keyof Incomes]}`)
+}
